5bded84f8c4924a1862cca5874fc4f3ab41b6aff
JoviJustino <joaovictortere@hotmail.com>
2023-02-16 12:57:11 -0300

    Adicionando o componente tag

--- src/pages/Details/Index.jsx
+++ src/pages/Details/Index.jsx
@@ -3,6 +3,7 @@ import { Container} from "./styles";
 import { Header } from "../../components/Header";
 import { Section} from "../../components/Section";
 import { Links } from "../../components/Section/styles.js";
+import { Tag } from "../../components/Tag";
 import { Button } from "../../components/Button";
 
 
@@ -14,9 +15,17 @@ export function Details() {
       <Section title="Links úteis">
 
       <Links>
-        <li><a href="https://github.com/JoviJustino" target="blank">Meu GitHub</a></li>
-        <li><a href="https://github.com/JoviJustino" target="blank">Meu GitHub</a></li>
+        <li><a href="https://github.com/JoviJustino" target="blank">https://github.com/JoviJustino</a></li>
+        <li><a href="https://github.com/JoviJustino" target="blank">https://github.com/JoviJustino</a></li>
       </Links>
+      </Section>
+
+      <Section title="Marcadores">
+
+
+      <Tag title="express"/>
+      <Tag title="Nodejs"/>
+
       </Section>
 
       <Button title="Entrar" loading />
