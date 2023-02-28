be2e572b4689700a641d05ba3bb98ab5f69232c1
JoviJustino <joaovictortere@hotmail.com>
2023-02-28 14:49:10 -0300

    Adicionando a página de New e criando o componente de TextArea

--- src/main.jsx
+++ src/main.jsx
@@ -8,14 +8,14 @@ import { Home } from "./pages/Home/index";
 import theme from "./pages/styles/theme";
 import { SignIn } from "./pages/SignIn";
 import { SignUp } from "./pages/SingUp";
-import { Profile } from "./pages/Profile";
+import { New } from "./pages/New";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Profile />
+      <New />
     </ThemeProvider>
   </React.StrictMode>
 );
